fdc9b679f39eb2793e5814d917946f9d3ea979d4
evertonhazevedo <hinckley.everton@gmail.com>
2023-05-28 17:15:56 -0300

    Front-End da aplicação 1

--- src/assets/js/btnDinamico.js
+++ src/assets/js/btnDinamico.js
@@ -34,20 +34,6 @@ document.addEventListener('DOMContentLoaded', function () {
 ;
 }, false);
 
-//Deslogar usuario
-document.getElementById('btnLogout')
-    .addEventListener('click', async function () {
-
-        localStorage.clear();
-
-        await Toast.fire({
-            icon: 'warning',
-            title: 'Encerrando sessão...'
-        })
-
-        window.location.href = "/index.html";
-    });
-
 //Sessão expirada
 document.addEventListener('click', function () {
 
